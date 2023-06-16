988595c211512c6035141d1004bd50a775c2851a
Melchs40 <samscodingjourney@gmail.com>
2023-06-16 15:34:33 -0500

    Edit regex styling in index.html, test.js

--- test.js
+++ test.js
@@ -10,5 +10,19 @@ password.addEventListener("input", function () {
     if (numberCheck === true) {
         document.getElementById("lowercase").style.color = "green";
     } else document.getElementById("lowercase").style.color = "black";
+});
+
+password.addEventListener("input", function () {
+    let numberCheck = /[A-Z]/.test(password.value);
+    if (numberCheck === true) {
+        document.getElementById("capital").style.color = "green";
+    } else document.getElementById("capital").style.color = "black";
+});
+
+password.addEventListener("input", function () {
+    let numberCheck =/^.{8,16}$/.test(password.value);
+    if (numberCheck === true) {
+        document.getElementById("length").style.color = "green";
+    } else document.getElementById("length").style.color = "black";
     
 });
