263e7710fcc01d47afd36204203933311c35c132
Melchs40 <samscodingjourney@gmail.com>
2023-06-16 15:30:47 -0500

    Edit regex styling in index.html, styles.css, test.js

--- test.js
+++ test.js
@@ -1,3 +1,14 @@
 document.getElementById("submit-button").addEventListener("click", function(){
 	document.getElementById("my-form").className="submitted";
 });
+
+
+let password = document.getElementById("first-password");
+
+password.addEventListener("input", function () {
+    let numberCheck = /[a-z]/.test(password.value);
+    if (numberCheck === true) {
+        document.getElementById("lowercase").style.color = "green";
+    } else document.getElementById("lowercase").style.color = "black";
+    
+});
